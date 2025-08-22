48d2a9b151b63ee20fae2a67f0e3856c3db62360
Yoon KyoungMin <yoongang02@gmail.com>
2025-08-22 23:56:44 +0900

    #3 Feat : UIManager, UIBase 생성
    - 모든 UI의 공유기능인 UIBase 생성
    - 모든 UI를 관리할 UIManager 생성

---
+++ Assets/02.Scripts/Managers/UIManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance {  get; private set; }
+    public UIBase topUI;
+
+    private Stack<UIBase> _uiStack = new Stack<UIBase>();
+
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void OpenUI(UIBase ui)
+    {
+        if (ui == null) return;
+
+        _uiStack.Push(ui);
+        topUI = ui;
+        ui.OnOpen();
+    }
+
+    public void CloseTopUI()
+    {
+        if (_uiStack.Count == 0) return;
+
+        UIBase topUI = _uiStack.Pop();
+
+        topUI.OnClose();
+
+        if (_uiStack.Count > 0)
+        {
+            this.topUI = _uiStack.Peek();
+        }
+        else
+        {
+            this.topUI = null;
+        }
+    }
+
+    public void CloseAllUI()
+    {
+        while (_uiStack.Count > 0)
+        {
+            CloseTopUI();
+        }
+    }
+
+    public UIBase GetTopUI()
+    {
+        if (_uiStack.Count == 0) return null;
+        return _uiStack.Peek();
+    }
+
+    public bool IsAnyUIOpen()
+    {
+        return _uiStack.Count > 0;
+    }
+
+    public bool IsUIOpen(UIBase ui)
+    {
+        return _uiStack.Contains(ui);
+    }
+
+}
