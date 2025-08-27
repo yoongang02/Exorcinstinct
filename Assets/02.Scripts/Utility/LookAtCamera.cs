8e91e86c81e44177916180b34c275459a7a3e56a
Yoon KyoungMin <yoongang02@gmail.com>
2025-08-28 01:10:34 +0900

    #5 : world canvas ui 카메라 바라보게 설정

---
+++ Assets/02.Scripts/Utility/LookAtCamera.cs
@@ -0,0 +1,11 @@
+﻿using DG.Tweening;
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    private void LateUpdate()
+    {
+        transform.LookAt(Camera.main.transform.position);
+    }
+}
