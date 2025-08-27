5c15720a0fd7d368113fec391d61e6e03fd4be43
Yoon KyoungMin <yoongang02@gmail.com>
2025-08-28 00:41:43 +0900

    #5 : 마우스 움직임에 따라 카메라 회전 구현
    Start에서 오일러 값 초기화해서, 시작할 때 튀는 현상 방지
    
    Cursor.lockState = CursorLockMode.Locked;
    Cursor.visible = true;
    -> 화면 중앙에 마우스 좌표 고정하고 커서는 보이게해서
    마우스가 중앙에 있을 때 디폴트 시야로 보이고 중앙 기준으로 카메라 회전되게 함

---
+++ Assets/02.Scripts/Utility/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    [SerializeField] private Vector2 _xRange;
+    [SerializeField] private Vector2 _yRange;
+
+    private float _mouseX;
+    private float _mouseY;
+
+    private void Start()
+    {
+        Vector3 init = transform.localEulerAngles;
+        _mouseX = init.y;
+        _mouseY = init.x;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = true;
+    }
+
+    void Update()
+    {
+        RotateCamera();
+    }
+
+    void RotateCamera()
+    {
+        _mouseX += Input.GetAxisRaw("Mouse X") * mouseSensitivity * Time.deltaTime;
+        _mouseY -= Input.GetAxisRaw("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        _mouseX = Mathf.Clamp(_mouseX, _xRange.x, _xRange.y);
+        _mouseY = Mathf.Clamp(_mouseY, _yRange.x, _yRange.y);
+
+        transform.localRotation = Quaternion.Euler(_mouseY, _mouseX, 0f);
+    }
+}
