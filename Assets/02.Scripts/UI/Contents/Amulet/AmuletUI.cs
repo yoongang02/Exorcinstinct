d8faa7a737fa53faa367200226b1d5ac8b81f950
Yoon KyoungMin <yoongang02@gmail.com>
2025-08-27 15:26:42 +0900

    #3 Feat : 부적 툴팁 연결 & 공용 툴팁 스크립트 작성
    - ToolTip 스크립트 부착하면 툴팁 기능 구현되도록 공용 스크립트 작성
    - 커서 왼쪽에 나타날지 오른쪽에 나타날지 선택 가능
    - 제목이 있는 버전인지 없는 버전인지 선택 가능

---
+++ Assets/02.Scripts/UI/Contents/Amulet/AmuletUI.cs
@@ -0,0 +1,89 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmuletUI : UIBase
+{
+    [Header("AmuletUI")]
+    [Space(5)]
+    [SerializeField] private TextMeshProUGUI _nameText;
+    [SerializeField] private Image _portraitImage;
+    [SerializeField] private TextMeshProUGUI _locationText;
+    [SerializeField] private TextMeshProUGUI _causeText;
+    [SerializeField] private Sprite _emptySprite;
+    [SerializeField] Button CheckAnswerBtn;
+
+    public override void OnOpen()
+    {
+        base.OnOpen();
+        transform.GetChild(0).gameObject.SetActive(true);
+    }
+
+    public override void OnClose()
+    {
+        base.OnClose();
+        transform.GetChild(0).gameObject.SetActive(false);
+    }
+
+    /// <summary>
+    /// 명단, 지도 등에서 호출하는 함수. 정보 세팅을 할 수 있다.
+    /// </summary>
+
+    public void SetStudent(StudentSO student)
+    {
+        _nameText.text = student.label;
+        _portraitImage.sprite = Resources.Load<Sprite>(student.sprite);
+    }
+
+    public void SetLocation(LocationSO location)
+    {
+        string str = $"{location.floor} {location.direction} {location.label}";
+        _locationText.text = str;
+    }
+
+    public void SetCause(CauseSO cause)
+    {
+        _causeText.text = cause.label;
+    }
+
+    /// <summary>
+    /// 정보를 초기화하는 함수
+    /// </summary>
+
+    public void ClearStudent()
+    {
+        _nameText.text = string.Empty;
+        _portraitImage.sprite = _emptySprite;
+    }
+
+    public void ClearLocation()
+    {
+        _locationText.text = string.Empty;
+    }
+
+    public void ClearCause()
+    {
+        _causeText.text = string.Empty;
+    }
+
+    /// <summary>
+    /// 현재 각 슬롯의 정보가 비어있는지 확인하는 함수.
+    /// 비어있을 경우에만 마우스 호버 시 툴팁이 뜨도록 하기 위함.
+    /// </summary>
+    /// <returns></returns>
+
+    public bool IsStudentSet()
+    {
+        return !string.IsNullOrEmpty(_nameText.text);
+    }
+
+    public bool IsLocationSet()
+    {
+        return !string.IsNullOrEmpty(_locationText.text);
+    }
+
+    public bool IsCauseSet()
+    {
+        return !string.IsNullOrEmpty(_causeText.text);
+    }
+}
---
+++ Assets/02.Scripts/Utility/ToolTip.cs
@@ -0,0 +1,103 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ToolTip : MonoBehaviour
+{
+    [SerializeField] private bool _isLeft = true; // true면 왼쪽, false면 오른쪽
+    [SerializeField] private bool _hasTitle = true; // true면 ObjectInfoSO의 title 사용, false면 ObjectInfoSO의 description만 사용
+    [SerializeField] private ObjectInfoSO _objectInfo;
+    [SerializeField] private GameObject _toolTip;
+    [SerializeField] private Vector2 _offset = new Vector2(0f, -100f); // 커서와 툴팁 사이의 간격
+
+    private Canvas _toolTipCanvas;
+    private RectTransform _toolTipRectTransform;
+
+    private void Awake()
+    {
+        _toolTipCanvas = GetComponentInParent<Canvas>();
+        _toolTipRectTransform = _toolTipCanvas.GetComponent<RectTransform>();
+    }
+
+    private void LateUpdate()
+    {
+        if (_toolTip.activeSelf)
+        {
+            if(_isLeft)
+                CalculateMousePositionLeft();
+            else
+                CalculateMousePositionRight();
+        }
+    }
+
+    public void ShowToolTip()
+    {
+        SetToolTipText();
+        _toolTip.SetActive(true);
+    }
+
+    public void HideToolTip()
+    {
+        _toolTip.SetActive(false);
+    }
+
+    private void SetToolTipText()
+    {
+        if (_hasTitle)
+        {
+            TextMeshProUGUI name = _toolTip.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+            TextMeshProUGUI description = _toolTip.transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+
+            name.text = _objectInfo.objectName;
+            description.text = _objectInfo.objectDescription;
+        }
+        else
+        {
+            TextMeshProUGUI description = _toolTip.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+            description.text = _objectInfo.objectDescription;
+        }
+    }
+
+    /// <summary>
+    /// 마우스 커서 위치에 따라 툴팁 위치를 왼쪽으로 조정하는 함수
+    /// </summary>
+    private void CalculateMousePositionLeft()
+    {
+        // 캔버스 좌표로 변환
+        Vector2 mousePos = Input.mousePosition;
+        Vector2 localPos;
+
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            _toolTipRectTransform, mousePos, _toolTipCanvas.worldCamera, out localPos);
+
+        // 툴팁 RectTransform
+        RectTransform rt = _toolTip.transform as RectTransform;
+
+        // 피벗을 (1,0.5)로 설정 (오른쪽 중앙)
+        rt.pivot = new Vector2(1f, 0.5f);
+
+        // 위치 지정
+        rt.anchoredPosition = localPos + _offset;
+    }
+
+    /// <summary>
+    /// 마우스 커서 위치에 따라 툴팁 위치를 오른쪽으로 조정하는 함수
+    /// </summary>
+    private void CalculateMousePositionRight()
+    {
+        // 캔버스 좌표로 변환
+        Vector2 mousePos = Input.mousePosition;
+        Vector2 localPos;
+
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            _toolTipRectTransform, mousePos, _toolTipCanvas.worldCamera, out localPos);
+
+        // 툴팁 RectTransform
+        RectTransform rt = _toolTip.transform as RectTransform;
+
+        // 피벗을 (0,0.5)로 설정 (왼쪽 중앙)
+        rt.pivot = new Vector2(0f, 0.5f);
+
+        // 위치 지정
+        rt.anchoredPosition = localPos + _offset;
+    }
+}
