a545e19fcae591f445ba37fc77b567dd4f75fad1
Yoon KyoungMin <yoongang02@gmail.com>
2025-09-01 23:51:05 +0900

    #6 Feat : 촛불 커짐, 켜짐 애니메이션 구현

---
+++ Assets/02.Scripts/CandleController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CandleController : MonoBehaviour
+{
+    [SerializeField] private float _smokeSeconds = 3f;
+    [SerializeField] private GameObject _flame;
+    [SerializeField] private GameObject _light;
+    [SerializeField] private GameObject _smoke;
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void LightOn()
+    {
+        _smoke.SetActive(false);
+        _animator.SetTrigger("LightOn");
+    }
+
+    public void LightOff()
+    {
+        _animator.SetTrigger("LightOff");
+    }
+
+    public void SmokeOn()
+    {
+        _flame.SetActive(false);
+        _light.SetActive(false);
+        _smoke.SetActive(true);
+        //StartCoroutine(SmokeOff());
+    }
+
+    IEnumerator SmokeOff()
+    {
+        yield return new WaitForSeconds(_smokeSeconds);
+        _smoke.SetActive(false);
+    }
+}
---
+++ Assets/02.Scripts/Item/MatchItem/MatchItem.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class MatchItem : UIBase, IItemBase
+{
+    [SerializeField] private GameObject _askWindow;
+    public override void OnOpen()
+    {
+        base.OnOpen();
+        _askWindow.SetActive(true);
+    }
+
+    public override void OnClose()
+    { 
+        base.OnClose(); 
+    }
+
+    public void UseItem()
+    {
+        
+    }
+
+    public void OnClickYesBtn()
+    {
+
+    }
+
+    public void OnClickNoBtn()
+    {
+
+    }
+}
