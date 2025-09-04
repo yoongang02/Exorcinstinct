c8dc91e12e81612c5ae01259d70936c582888b25
Yoon KyoungMin <yoongang02@gmail.com>
2025-09-04 13:18:45 +0900

    #7 Feat : 시작, 마이크 체크, 인트로, 메인 씬 연결

---
+++ Assets/02.Scripts/Start/StartScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScene : MonoBehaviour
+{
+    public void OnClickStartBtn()
+    {
+        SceneChanger.Instance.ChangeScene("MikeCheckScene").Forget();
+    }
+
+    public void OnClickQuitBtn()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/02.Scripts/Utility/SceneChanger.cs
@@ -0,0 +1,66 @@
+﻿using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public static SceneChanger Instance { get; private set; }
+    public CanvasGroup _fadeImg;
+    float fadeDuration = 1.5f;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        _fadeImg = transform.GetComponentInChildren<CanvasGroup>();
+        _fadeImg.DOFade(0, fadeDuration)
+            .OnComplete(() =>
+            {
+                _fadeImg.blocksRaycasts = false;
+            });
+    }
+
+    public async UniTaskVoid ChangeScene(string sceneName)
+    {
+        _fadeImg = transform.GetComponentInChildren<CanvasGroup>();
+        await _fadeImg.DOFade(1, fadeDuration)
+            .OnStart(() => {
+                _fadeImg.blocksRaycasts = true;
+                //SoundManager.Instance.StopBGM();
+            })
+            .AsyncWaitForCompletion();
+
+        await LoadScene(sceneName);
+    }
+
+    private async UniTask LoadScene(string sceneName)
+    {
+        AsyncOperation async = SceneManager.LoadSceneAsync(sceneName);
+
+        while (!async.isDone)
+        {
+            await UniTask.Yield();
+        }
+    }
+}
