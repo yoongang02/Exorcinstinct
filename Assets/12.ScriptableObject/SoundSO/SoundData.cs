e04dbb4c6fd89dc0d3a1511f67b5aeee652463df
Yoon KyoungMin <yoongang02@gmail.com>
2025-09-04 18:16:31 +0900

    #7 Feat : 사운드 매니저 생성 & 타임라인, blood 패키지 임포트

---
+++ Assets/12.ScriptableObject/SoundSO/SoundData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Sound Data", menuName = "Sound/Create Sound Data")]
+public class SoundData : ScriptableObject
+{
+    public string soundName; //sound 아이디
+    public AudioClip clip;  //sound clip
+    public int loopCnt; //루프 횟수
+    public float volume; //볼륨
+}
